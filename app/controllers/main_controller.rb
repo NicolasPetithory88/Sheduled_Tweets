f50ec000e215bbd1542fa9d73a602d606dbecf0b
NICOLASPETITHORY88 <NICOLAS4.PETITHORY@GMAIL.com>
2022-12-27 18:26:11 +0100

    passwords

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,5 @@
 class MainController < ApplicationController
     def index
-        flash.now[:notice] = "logged in successfully"
-        flash.now[:alert] = "invalid email or password "
+
     end
 end
